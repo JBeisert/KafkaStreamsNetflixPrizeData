1627695034e65ce07ca4ce7c3e4634916866a7c1
JBeisert <beisert97@gmail.com>
2024-06-06 09:41:10 +0200

    v1

--- KafkaFlightStreams/scripts/03_run_kafka_producer.sh
+++ KafkaFlightStreams/scripts/03_run_kafka_producer.sh
@@ -3,5 +3,9 @@
 CLUSTER_NAME=$(/usr/share/google/get_metadata_value attributes/dataproc-cluster-name)
 
 java -cp /usr/lib/kafka/libs/*:KafkaProducer.jar \
+com.example.bigdata.TestProducer data/movie_titles 0 movie-info-input \
+0 "${CLUSTER_NAME}"-w-0:9092
+
+java -cp /usr/lib/kafka/libs/*:KafkaProducer.jar \
 com.example.bigdata.TestProducer data/netflix-prize-data 10 netflix-ratings-input \
 1 "${CLUSTER_NAME}"-w-0:9092
